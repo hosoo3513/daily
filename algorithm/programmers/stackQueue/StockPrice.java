a08e64ef45896625910cae6b8bee49461c8861d9
HOSOO KANG <hosoo3513@gmail.com>
2022-10-15 20:29:45 +0900

    10/15 refactoring

--- algorithm/programmers/stackQueue/StockPrice.java
+++ algorithm/programmers/stackQueue/StockPrice.java
@@ -5,21 +5,15 @@ import java.util.List;
 
 public class StockPrice {
     public int[] solution(int[] prices) {
-        List<Integer> list = new ArrayList<>();
+        int len = prices.length;
+        int[] answer = new int[len];
 
-        // i번째와 j번째를 비교한다.
-        for (int i = 0; i < prices.length-1; i++) {
-            int num = 0;
-            // 비교를 하는 순간 num을 1 더해준다 -> 1초 뒤에 가격이 떨어진 것이라 생각하기 때문
-            for (int j = i + 1; j < prices.length; j++) {
-                num++;
+        for (int i = 0; i < len-1; i++) {
+            for (int j = i + 1; j < len; j++) {
+                answer[i]++;
                 if (prices[i] > prices[j]) break;
             }
-            list.add(num);
         }
-        list.add(0);
-
-        int[] answer = list.stream().mapToInt(i -> i).toArray();
 
         return answer;
     }
