f753b6b01aab7b802b244eb1163e9ce49a9027c8
강호수 <hosoo3513@gmail.com>
2022-11-13 23:53:29 +0900

    11/13

--- algorithm/programmers/greedy/GymSuit.java
+++ algorithm/programmers/greedy/GymSuit.java
@@ -1,4 +1,39 @@
 package algorithm.programmers.greedy;
 
+import java.util.Arrays;
+
 public class GymSuit {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int cnt = 0;
+        boolean[] visit = new boolean[reserve.length];
+
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+
+        for (int i = 0; i < lost.length; i++) {
+            for (int j = 0; j < reserve.length; j++) {
+                if (lost[i] == reserve[j]) {
+                    cnt++;
+                    visit[j] = true;
+                    break;
+                }
+            }
+        }
+
+
+        for (int i = 0; i < lost.length; i++) {
+            for (int j = 0; j < reserve.length; j++) {
+                if (visit[j] == false && (lost[i] == reserve[j] - 1 || lost[i] == reserve[j] + 1)) {
+                    visit[j] = true;
+                    cnt++;
+                    break;
+                }
+            }
+        }
+
+        int no = lost.length - cnt;
+
+        return n - no;
+    }
+
 }
