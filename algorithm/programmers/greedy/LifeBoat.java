5656c4f3655d7672a98752fda24f1753b0a648b6
HOSOO KANG <hosoo3513@gmail.com>
2022-11-20 00:56:52 +0900

    11/20

--- algorithm/programmers/greedy/LifeBoat.java
+++ algorithm/programmers/greedy/LifeBoat.java
@@ -1,6 +1,9 @@
 package algorithm.programmers.greedy;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 70 50 80 50 -> 4
@@ -18,11 +21,50 @@ public class LifeBoat {
 
         Arrays.sort(people);
 
-        for (int i = 0; i < people.length; i++) {
-            while()
-        }
+        int min = 0;
 
+        for (int max = people.length - 1; min <= max; max--) {
+            if (people[min] + people[max] <= limit) min++;
+            answer++;
+        }
+        return answer;
+    }
+
+    public int solution2(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        List<Integer> p = Arrays.stream(people).boxed().collect(Collectors.toList());
+
+        while (p.size()>0) {
+
+            int a = p.size() - 1;
+            while (p.get(0) + p.get(a) > limit && a-- > 0) {}
+            if (a == 0) {
+                break;
+            } else {
+                answer++;
+                p.remove(a);
+                p.remove(0);
+            }
+        }
+        for (int a : p) {
+            if (a <= limit) {
+                answer++;
+            }
+        }
 
         return answer;
     }
+
+    public static void main(String[] args) {
+        LifeBoat L = new LifeBoat();
+        int[] people = new int[]{30,50,50,70};
+        int[] people2 = new int[]{70, 50, 80};
+
+        System.out.println(L.solution(people, 100));
+        System.out.println(L.solution(people2, 100));
+
+    }
+
+
 }
