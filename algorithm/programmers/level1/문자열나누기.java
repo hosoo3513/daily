f00cc00c0428a9572ac168ed240e155d92d18f17
HOSOO KANG <hosoo3513@gmail.com>
2022-12-05 17:23:44 +0900

    12/5

---
+++ algorithm/programmers/level1/문자열나누기.java
@@ -0,0 +1,41 @@
+package algorithm.programmers.level1;
+
+import java.util.HashMap;
+
+public class 문자열나누기 {
+    public int solution(String s) {
+        int answer = 0;
+
+        char[] chars = s.toCharArray();
+        int a = 0;
+
+        while (a < s.length()) {
+            HashMap<Character, Integer> map = new HashMap<>();
+            map.put(chars[a], map.getOrDefault(chars[a], 0) + 1);
+
+            int tmp = 0;
+
+            for (int i = a + 1; i < chars.length; i++) {
+                if (chars[i] == chars[a]) {
+                    map.put(chars[a], map.get(chars[a]) + 1);
+                } else {
+                    tmp++;
+                }
+
+                if (map.get(chars[a]) == tmp) {
+                    answer++;
+                    a = i + 1;
+                    break;
+                }
+            }
+
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        문자열나누기 m = new 문자열나누기();
+        System.out.println(m.solution("abracadabra"));
+    }
+}
