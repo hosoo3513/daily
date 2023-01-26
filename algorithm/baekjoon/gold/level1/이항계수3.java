94ded425af91bf66420a1c68aca2228c5d89f2e4
강호수 <hosoo3513@gmail.com>
2023-01-27 02:11:02 +0900

    1/27, 모듈러 성질 이용

--- algorithm/baekjoon/gold/level1/이항계수3.java
+++ algorithm/baekjoon/gold/level1/이항계수3.java
@@ -3,20 +3,43 @@ package algorithm.baekjoon.gold.level1;
 import java.util.Scanner;
 
 public class 이항계수3 {
+
+    final static long P = 1000000007;
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
         int N = scan.nextInt();
         int K = scan.nextInt();
 
-        int tmp = 1;
-        for (int i = 0; i < K; i++) {
-            tmp *= N--;
-        }
-        for (int i = 1; i <= K; i++) {
-            tmp /= i;
-        }
+        long numer = factorial(N) % P;
+        long denom = (factorial(K) * factorial(N - K)) % P;
 
-        System.out.println(tmp % 1000000007);
+        System.out.println((numer * pow(denom, P - 2)) % P);
+    }
+
+    public static long factorial(long N) {
+        long fac = 1L;
+
+        while (N > 1) {
+            fac = (fac * N) % P;
+            N--;
+        }
+        return fac;
+    }
+
+    public static long pow(long base, long expo) {
+
+        long result = 1;
+
+        while (expo > 0) {
+            if (expo % 2 == 1) {
+                result *= base;
+                result %= P;
+            }
+            base = (base * base) % P;
+            expo /= 2;
+        }
+        return result;
     }
 }
