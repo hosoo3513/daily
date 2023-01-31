a03ea2523bccc8f6ff5fc9f6d458bddc3a6cdd22
강호수 <hosoo3513@gmail.com>
2023-02-01 01:22:06 +0900

    2/1

--- algorithm/baekjoon/silver/level4/체스판다시칠하기.java
+++ algorithm/baekjoon/silver/level4/체스판다시칠하기.java
@@ -3,45 +3,64 @@ package algorithm.baekjoon.silver.level4;
 import java.util.Scanner;
 
 public class 체스판다시칠하기 {
+
+    static char[][] board;
+    static int min = Integer.MAX_VALUE;
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int row = scan.nextInt();
         int col = scan.nextInt();
 
-        String[] board = new String[row];
+        board = new char[row][col];
 
         for (int i = 0; i < row; i++) {
-            board[i] = scan.next();
+            String next = scan.next();
+            for (int j = 0; j < next.length(); j++) {
+                board[i][j] = next.charAt(j);
+            }
         }
+
+        for (int i = 0; i < row - 7; i++) {
+            for (int j = 0; j < col - 7; j++) {
+                getDifference(i, j);
+            }
+        }
+
+        System.out.println(min);
+    }
+
+    private static void getDifference(int row, int col) {
+
 
         int difference = 0;
 
-        for (int i = 0; i < row; i += 2) {
-            for (int j = 0; j < col; j += 2) {
-                if (board[i].charAt(j) != 'B') {
+        for (int i = row; i < row + 8; i += 2) {
+            for (int j = col; j < col + 8; j += 2) {
+                if (board[i][j] != 'B') {
                     difference++;
                 }
             }
-            for (int j = 1; j < col; j += 2) {
-                if (board[i].charAt(j) != 'W') {
-                    difference++;
-                }
-            }
-        }
-
-        for (int i = 1; i < row; i += 2) {
-            for (int j = 0; j < col; j += 2) {
-                if (board[i].charAt(j) != 'W') {
-                    difference++;
-                }
-            }
-            for (int j = 1; j < col; j += 2) {
-                if (board[i].charAt(j) != 'B') {
+            for (int j = col + 1; j < col + 9; j += 2) {
+                if (board[i][j] != 'W') {
                     difference++;
                 }
             }
         }
 
-        System.out.println(Math.min(difference, row * col - difference));
+        for (int i = row + 1; i < row + 9; i += 2) {
+            for (int j = col; j < col + 8; j += 2) {
+                if (board[i][j] != 'W') {
+                    difference++;
+                }
+            }
+            for (int j = col + 1; j < col + 9; j += 2) {
+                if (board[i][j] != 'B') {
+                    difference++;
+                }
+            }
+        }
+
+        min = Math.min(min, Math.min(difference, 64 - difference));
     }
 }
