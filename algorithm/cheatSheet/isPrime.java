f466c9a28a738c6bc13eb3431665a535b2163637
강호수 <hosoo3513@gmail.com>
2023-02-06 12:21:30 +0900

    fix: isPrime 수정

--- algorithm/cheatSheet/isPrime.java
+++ algorithm/cheatSheet/isPrime.java
@@ -2,8 +2,9 @@ package algorithm.cheatSheet;
 
 public class isPrime {
     public boolean isPrime(int num) {
-        if (num==0 || num==1) return false;
-        for (int i = 3; i <= (int) Math.sqrt(num); i+=2) {
+        if (num < 2) return false;
+        if (num == 2) return true;
+        for (int i = 2; i <= (int) Math.sqrt(num); i++) {
             if (num % i == 0) return false;
         }
         return true;
