72b62400f3a5dc29d7d1302cf700424408569c46
강호수 <hosoo3513@gmail.com>
2023-02-24 19:28:49 +0900

    2/24

---
+++ algorithm/baekjoon/bronze/level1/수정렬하기3.java
@@ -0,0 +1,30 @@
+package algorithm.baekjoon.bronze.level1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class 수정렬하기3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[num];
+
+        for (int i = 0; i < num; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i : arr) {
+            sb.append(i).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
